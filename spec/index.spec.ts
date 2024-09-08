b61e81c087e40fa1ea6a8622017392d4383be079
prakhar3062 <prakharm.bt.20@nitj.ac.in>
2024-09-08 16:28:29 +0530

    upadate index.spec

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -5,33 +5,33 @@ import './addBook.spec';
 
 
 // This simulates a pool query that can return a predefined result
-// mocks/mockPool.ts
+
 
 export const mockPoolQuery = async (query: string, params: any[]) => {
   // Handling specific query patterns
   if (query.includes("SELECT") && query.includes("FROM books")) {
-    // Simulate a SELECT query for books
+    
     return Promise.resolve({
       rows: [{ id: 1, title: "Mock Book", author: "Mock Author", genre: "Fiction", publicationYear: 2024 }]
     });
   }
 
   if (query.includes("INSERT INTO books")) {
-    // Simulate an INSERT query
+    
     return Promise.resolve({
-      rowCount: 1 // Simulates a successful insert
+      rowCount: 1 
     });
   }
 
   if (query.includes("UPDATE books")) {
     // Simulate an UPDATE query
     return Promise.resolve({
-      rowCount: 1 // Simulates a successful update
+      rowCount: 1 
     });
   }
 
   if (query.includes("SELECT") && query.includes("FROM users")) {
-    // Simulate a SELECT query for users
+    
     return Promise.resolve({
       rows: [{ id: 1, username: "testuser", password: "hashed_password" }]
     });
